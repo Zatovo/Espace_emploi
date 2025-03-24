db40d03fb4a8c818bacb34b7c1f73ee7cf477cfb
Zatovo <rakotonirinazatovo@gmail.com>
2025-03-24 11:52:03 +0300

    feat : add tools

--- app/Models/Offre.php
+++ app/Models/Offre.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @mixin IdeHelperOffre
+ */
 class Offre extends Model
 {
     use HasFactory;
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @mixin IdeHelperUser
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
@@ -25,4 +28,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function session(): BelongsTo
+    {
+        return $this->belongsTo(Session::class);
+    }
+
+    public function candidature(): HasMany
+    {
+        return $this->hasMany(Candidature::class);
+    }
+
+    public function offre(): HasMany
+    {
+        return $this->hasMany(Offre::class);
+    }
 }
